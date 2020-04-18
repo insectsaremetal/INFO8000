17e83e6642981aba37e0e150b2a182fa51eac850
Heather Gaya <Heather.e.gaya@gmail.com>
2020-04-18 18:26:21 -0400

    Allows for 2020 predictions

---
+++ Semester_Project/Shinyapp_files/server.R
@@ -0,0 +1,108 @@
+# Define server logic ----
+server <- function(input, output, session) {
+  session$onSessionEnded(function() {
+    stopApp()
+  })
+  
+  output$title <- renderText({"Predicted Southern Pine Beetle Outbreaks"})
+  
+  output$file <- renderPlot({
+    Outbreakdata <<- read.csv("Outbreak_data.csv")
+    Outbreakdata2 <- usmap_transform(Outbreakdata[,c("Longitude", "Latitude", "Year")])
+    Outbreakdata2$Year <- as.factor(Outbreakdata2$Year)
+    Years <- switch(input$YearButton, "2016" = "2016", "2017" = "2017", 
+                    "2018" = "2018", "All" = Outbreakdata2$Year)
+    Outbreakdata3 <- Outbreakdata2[Outbreakdata2$Year %in% Years, ]
+    Fakedata <- data.frame(Longitude = as.numeric(input$lon),Latitude = as.numeric(input$lat))
+    Fakedata2 <- usmap_transform(Fakedata)
+    
+    G1 <<- plot_usmap("states", 
+               include = c(.south_region), exclude= c("Texas", "Oklahoma"))+ 
+    geom_point(data = Outbreakdata3, aes(x = Longitude.1, y= Latitude.1, col = Year))+
+      scale_color_discrete(drop=FALSE)+
+      geom_point(data = Fakedata2, aes(x = Longitude.1, y = Latitude.1), pch = 4, size =3)
+    
+    G1
+  })
+  
+  observeEvent(input$coords, {
+    Fakedata <<- data.frame(Longitude = input$lon, Latitude = input$lat, Year = input$predyear)
+    if(input$predyear != 2020){
+    daymet <- download_daymet(site = "mysite",
+                              lat = Fakedata$Latitude,
+                              lon = Fakedata$Longitude,
+                              start = input$predyear -1,
+                              end = input$predyear,
+                              internal = TRUE,
+                              simplify = F) # returns tidy data! 
+    Fakedata$JANMINTEMP <- mean(daymet$data$tmin..deg.c.[366:396]) #2019
+    Fakedata$FEBMINTEMP <- mean(daymet$data$tmin..deg.c.[397:424]) #2019
+    Fakedata$MARMINTEMP <- mean(daymet$data$tmin..deg.c.[425:455]) #2019
+    Fakedata$Prcp <- sum(daymet$data$prcp..mm.day.[365:730]) #2019
+    Fakedata$AUGMAXTEMP <- mean(daymet$data$tmax..deg.c.[213:243]) #2018!!
+    }
+    
+if(input$predyear == 2020){
+      predme <- Fakedata
+      coordinates(predme) <- ~Longitude+Latitude
+      Jan <<- raster("2020_Jan_min.grd")
+      Feb <<- raster("2020_Feb_min.grd")
+      March <<- raster("2020_March_min.grd")
+      Aug <<- raster("2019_August_max.grd")
+      Precip <<- raster("2019_ppt.grd")
+      
+      Fakedata$JANMINTEMP <- extract(Jan, predme)
+      Fakedata$FEBMINTEMP <- extract(Feb, predme)
+      Fakedata$MARMINTEMP <- extract(March, predme)
+      Fakedata$Prcp <- extract(Precip, predme)
+      Fakedata$AUGMAXTEMP <- extract(Aug, predme)
+    }
+    
+    m_gam1 <<- readRDS("insect_predict.rds")
+    preds <- predict(m_gam1,Fakedata,type="response", se.fit = T)
+    output$predicted <- renderText({paste("Current Model Predicts a ", round(preds$fit, 2), "Acre Outbreak in", Fakedata$Year, "with SE = ", round(preds$se.fit,2)) })
+    output$predictors <- renderText({paste("Predictions based on January min temp of", round(Fakedata$JANMINTEMP, digits = 0), "\u00B0C, February min temp of", round(Fakedata$FEBMINTEMP, digits = 0), "\u00B0C, March min temp of  ", round(Fakedata$MARMINTEMP, digits = 0), "\u00B0C, August max temperature of ", round(Fakedata$AUGMAXTEMP, digits = 0), "\u00B0C, and annual precipitation of", round(Fakedata$Prcp, digits =2), "mm")})
+  })
+
+  
+  observeEvent(input$button, {
+    if (input$user == "Insects" & input$PW == "RCool" & !is.null(input$file))
+      {newdat <<- read.csv(isolate({input$file})$datapath)
+       output$uploaded <- renderText({"File Uploaded"})} 
+    else 
+         output$uploaded <- renderText({"Please log in and upload a .csv file to add data"})})
+  
+  observeEvent(input$optimize, {
+    if (!is.null(newdat)){
+    for (i in 1: nrow(newdat)){
+    daymet <- download_daymet(site = "mysite",
+                              lat = newdat$Latitude[i],
+                              lon = newdat$Longitude[i],
+                              start = newdat$Year[i] - 1,
+                              end = newdat$Year[i],
+                              internal = TRUE,
+                              simplify = F) # returns tidy data! 
+    newdat$JANMINTEMP[i] <- mean(daymet$data$tmin..deg.c.[366:396]) #my year
+    newdat$FEBMINTEMP[i] <- mean(daymet$data$tmin..deg.c.[397:424]) #my year
+    newdat$MARMINTEMP[i] <- mean(daymet$data$tmin..deg.c.[425:455]) #my year
+    newdat$Prcp[i] <- sum(daymet$data$prcp..mm.day.[365:730]) #my year 
+    newdat$AUGMAXTEMP[i] <- mean(daymet$data$tmax..deg.c.[213:243]) #year BEFORE 
+    } 
+      
+    #get all the variables for the new data
+    
+    alldata <-merge(newdat, Outbreakdata, all = T) #combine with previous data
+    write.csv(alldata, "Outbreak_data.csv") #write to file
+    m_gam  <- gam(Acres ~ 
+                     s(AUGMAXTEMP) + s(Prcp) +
+                     s(JANMINTEMP,FEBMINTEMP,MARMINTEMP) +
+                     s(Longitude, Latitude),
+                   family = nb(), data = alldata,
+                   method = "REML") #re-fit model
+    saveRDS(m_gam, "insect_predict.rds") #save it as new working model
+    output$runmodel <- renderText({"Model Re-Optimized!"})
+    }
+  })
+  
+  #add in a toggle to plot raw data/stuff 
+}
---
+++ Semester_Project/Shinyapp_files/ui.R
@@ -0,0 +1,64 @@
+library(shiny)
+library(shinyjs)
+library(readr)
+library(knitr)
+library(daymetr)
+library(raster)
+library(lattice)
+library(latticeExtra)
+library(mgcv)
+library(RCurl)
+library(rgdal)
+library(usmap)
+library(ggplot2)
+library(maptools)
+
+#setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #set directory to file location
+m_gam1 <<- readRDS("insect_predict.rds")
+#Fakedata <<- data.frame(Longitude = -85.45714, Latitude = 32.95954) #until new values are put in
+
+ui <<- fluidPage(
+  useShinyjs(),
+  tags$head(
+    tags$style(type = 'text/css', '#title{font-size: 48px; font-family: calibri light; 
+               background-color: rgba(255,255,255,0.40); color: blue; border-style: none;}')
+  ),
+  
+  fluidRow(
+    column(textOutput("title"), offset = 0, width = 12)),
+  
+  fluidRow(
+  column(radioButtons("YearButton", 
+                      "What Year of Data Would You Like to See?", 
+                      c("2016", "2017", "2018", "All")), width = 3),
+  column(width = 2, textOutput("predicted")),
+  column(width = 2, textOutput("predictors")),
+  column(plotOutput("file"), width = 5)),
+  
+  fluidRow(
+    column(width = 2,
+           numericInput("lat", "Latitude (DD)", value = 31.95954, min = 25, max = 39, step = .20000)),
+    column(width = 2,
+           numericInput("lon", "Longitude (DD)", value = -85.45714, min = -95, max = -76, step = .20000)),
+    column(width = 2,
+           numericInput("predyear", "Year (1990 - 2020)", value = 2020, min = 1990, max= 2020)),
+    column(width = 2,
+           actionButton("coords", "Predict"))),
+
+  fluidRow(
+  column(width = 4, fileInput("file", h3("Add Outbreak Data (requires log in)"), 
+           accept=c('text/csv', 'text/comma-separated-values,text/plain'))),
+  column(width = 3, textOutput("uploaded")),
+  column(width = 3, textOutput("runmodel"))),
+  
+  fluidRow(
+    column(width = 2,
+           textInput("user", "User Name", value = "example")),
+    column(width = 2,
+           passwordInput("PW", "Password", value = "password")),
+    column(width = 2,
+           actionButton("button", "Upload")),
+  column(width = 2,
+         actionButton("optimize", "Re-Run Model")),
+  tags$p("(this may take a few minutes)"))
+)
